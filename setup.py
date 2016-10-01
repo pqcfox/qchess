2d499ef74b72a84258f6f73d4394ed9a8ab35156
Chandler <use.an.alias@gmail.com>
2016-09-30 22:05:01 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='qchess',
+    version='1.0.0.dev1',
+    description='A simple quantum chess client.',
+    long_description=long_description,
+    url='https://github.com/useanalias/qchess',
+    author='Chandler',
+    author_email='use.an.alias@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Games/Entertainment :: Board Games',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='qchess quantum chess',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    entry_points={
+        'console_scripts': [
+            'qchess=qchess:main',
+        ],
+    },
+)
